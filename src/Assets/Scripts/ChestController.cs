010df0dbc9143743ed1e5051a362ffcc0e26e1b4
Christian A. Jacobsen <c.a.jacobsen95@gmail.com>
2018-02-10 14:27:55 +0000

    Added chest animation and basic chest script requiring both players to touch it for the chest to open.

---
+++ src/Assets/Scripts/ChestController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    private bool isOpen = false;
+    private int playerCount = 0;
+
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isOpen)
+        {
+            return;
+        }
+
+        if (other.tag == "Player")
+        {
+            playerCount++;
+            if (playerCount == 2)
+            {
+                isOpen = true;
+                animator.SetLayerWeight(1, 1.0f);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerCount--;
+        }
+    }
+
+}
